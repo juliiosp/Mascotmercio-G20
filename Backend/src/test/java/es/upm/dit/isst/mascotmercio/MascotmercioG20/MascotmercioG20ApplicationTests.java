f4691b197ea2a35a09d1e816ccd18921c0061cc9
Julio <jsanchezpajaresa@gmail.com>
2024-03-22 17:47:49 +0100

    Creamos la estructura con Spring Initializer

---
+++ Backend/src/test/java/es/upm/dit/isst/mascotmercio/MascotmercioG20/MascotmercioG20ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.upm.dit.isst.mascotmercio.MascotmercioG20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MascotmercioG20ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
