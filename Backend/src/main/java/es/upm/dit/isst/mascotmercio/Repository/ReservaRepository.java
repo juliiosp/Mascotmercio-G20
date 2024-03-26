cedb10c03a087b998c2d51c78b8ef9996aa50fb7
pruthjara <pruthjperez@gmail.com>
2024-03-26 21:21:07 +0000

    Crear todos los repositorios

--- Backend/src/main/java/es/upm/dit/isst/mascotmercio/Repository/ReservaRepository.java
+++ Backend/src/main/java/es/upm/dit/isst/mascotmercio/Repository/ReservaRepository.java
@@ -4,5 +4,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReservaRepository extends JpaRepository<Reserva, Integer> {
-    // Aquí puedes agregar métodos personalizados si necesitas consultas específicas
+    
 }
