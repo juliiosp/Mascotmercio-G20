1872d1de7b1bcd7cc9ce4db2c6f94c5ff4bc8102
Javier <javier.mcayunao@alumnos.upm.es>
2024-03-29 21:00:13 +0100

    DuenoEstablecimiento

---
+++ Backend/src/main/java/es/upm/models/DuenoEstablecimiento.java
@@ -0,0 +1,73 @@
+package es.upm.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class DuenoEstablecimiento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private long id;
+
+        private String nombre;
+		private int telefono;
+		private String direccion;
+		private String email;
+		private String contraseña;
+        
+    public DuenoEstablecimiento(){
+
+    }
+
+    public DuenoEstablecimiento(Long id, String nombre, int telefono, String direccion, String email, String contraseña){
+        
+        this.id = id;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.email = email;
+        this.contraseña = contraseña;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+}
