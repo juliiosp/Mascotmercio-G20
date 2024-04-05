fd89fba25f066848afb1def2b31212b32bc31446
Freddy <freddy.vmerma@gmail.com>
2024-04-05 21:31:20 +0200

    POST Establecimiento @ireneplaza @javiermcayunao @juliiosp @pruthjara

--- Frontend/src/Vista_Dueño_Establecimiento/AñadirEstablecimiento.js
+++ Frontend/src/Vista_Dueño_Establecimiento/AñadirEstablecimiento.js
@@ -1,27 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Añadir.css'; // Importa los estilos CSS
 
 const Añadir = () => {
+  const [formData, setFormData] = useState({
+    nombre: '',
+    telefono: '',
+    direccion: '',
+    email: ''
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('http://localhost:8080/api/establecimientos', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      // Puedes redirigir a la página principal o a otra página después de enviar el formulario
+      // Aquí te redirijo a la página de VistaPrincipal
+      // @juliiosp aqui hay que cambiar algo para que no nos lleve a la de error!!!
+      window.location.href = '/vistaPrincipal';
+    } catch (error) {
+      console.error('Error submitting form:', error);
+    }
+  };
+
   return (
     <div className="fondo-añadir" style={{ background: 'linear-gradient(to right, #93FAF6, #FFB1FF)' }}>
       <h1>Añade tu establecimiento!</h1>
-      <form className="añadir-formulario">
+      <form className="añadir-formulario" onSubmit={handleSubmit}>
         <div className="form-group">
           <label htmlFor="nombre">Nombre:</label>
-          <input type="text" id="nombre" name="nombre" />
+          <input type="text" id="nombre" name="nombre" value={formData.nombre} onChange={handleChange} />
         </div>
         <div className="form-group">
-          <label htmlFor="int">Teléfono:</label>
-          <input type="text" id="telefono" name="telefono" />
+          <label htmlFor="telefono">Teléfono:</label>
+          <input type="text" id="telefono" name="telefono" value={formData.telefono} onChange={handleChange} />
         </div>
         <div className="form-group">
-          <label htmlFor="text">Dirección:</label>
-          <input type="text" id="direccion" name="direccion" />
+          <label htmlFor="direccion">Dirección:</label>
+          <input type="text" id="direccion" name="direccion" value={formData.direccion} onChange={handleChange} />
         </div>
         <div className="form-group">
           <label htmlFor="email">Correo Electrónico:</label>
-          <input type="text" id="email" name="email" />
+          <input type="text" id="email" name="email" value={formData.email} onChange={handleChange} />
         </div>
         <button type="submit">Subir</button>
         <Link to="/duenoEstablecimiento">
